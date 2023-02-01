d212f308e2282662892f3605c25c57b5cec6a6f0
sofia28304 <sofia.vogelhaupt@yahoo.de>
2023-02-01 13:41:42 +0100

    branch update

--- frontend/src/components/AllStuff.js
+++ frontend/src/components/AllStuff.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react"
 
-
 export function ItemList() {
     const [inventar, setInventar] = useState([])
-
+    //console.log("inventar console log: ", inventar)
     useEffect(() => {
         fetch("http://localhost:9998/api/bigstuff")
             .then((response) => response.json())
-            .then((data) => { setInventar(data) })
+            .then((data) => {
+                setInventar(data)
+                console.log(data)
+            })
     }, [])
+
+    console.log(inventar)
     return (
         <div>
-            {inventar.map((inventar, key) => <article key={key}>
-                <p>Name: {inventar.titel}</p>
-                <p>Raum: {inventar.room}</p>
-                <p>Stuff: {inventar.stuff}</p>
-            </article>)}
+            {inventar?.furn?.map((inventar) =>
+                <article>
+                    <p>Name: {inventar.titel}</p>
+                    <p>Raum: {inventar.room}</p>
+                    <p>Stuff: {inventar.stuff}</p>
+                </article>
+            )}
         </div>
     )
 }
